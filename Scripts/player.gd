ad2a6c048fe323ad785ae4291b66c0e73ce46f4f
A-1 <a.arko06@gmail.com>
2023-03-29 09:29:34 +0300

    The Sang Update
    - Added Sang
    - Made Sang more Sangy

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -13,15 +13,22 @@ var direction = Vector3()
 var gravity_vec = Vector3()
 var movement = Vector3()
 
-#camera var
+#camera var and function
 var mouseDelta : Vector2 = Vector2()
 onready var camera : Camera = get_node("Position3D/Camera")
 var minLookAngle : float = -90.0
 var maxLookAngle : float = 90.0
 var lookSensitivity : float = 10.0
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouseDelta = event.relative
+
+func _ready():
+	#locks mouse	
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)  
 
 func _physics_process(delta):
-	
+
 	# camera, works
 	camera.rotation_degrees.x -= mouseDelta.y * lookSensitivity * delta
 	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, minLookAngle, maxLookAngle)
@@ -31,10 +38,10 @@ func _physics_process(delta):
 	# movement
 	direction = Vector3.ZERO
 	var h_rot = global_transform.basis.get_euler().y
-	var f_input = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	var f_input = Input.get_action_strength("move_back") - Input.get_action_strength("move_forward")
 	var h_input = Input.get_action_strength("strafe_right") - Input.get_action_strength("strafe_left")
 	direction = Vector3(h_input, 0, f_input).rotated(Vector3.UP, h_rot).normalized()
-	# movement smooth, could be interpolate or action_strenght
+	# movement smooth, could be interpolate or action_strength
 	velocity = velocity.linear_interpolate(direction * speed, accel * delta)
 	movement = velocity + gravity_vec
 	
@@ -44,16 +51,14 @@ func _physics_process(delta):
 	if not is_on_floor():
 		newton.y -= gravity * delta
 	move_and_slide(newton, Vector3.UP)
+	#Running
+	if Input.is_key_pressed(KEY_SHIFT):
+		speed = 30
+	else:
+		speed = 14
 
 
-func _ready():
-	#locks mouse
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)  
 
-func _input(event):
-	#no clue what this does /p
-	#we should probably comment this code out instead of adding comments ~ A-1
-	#my bad /p
-	if event is InputEventMouseMotion:
-		mouseDelta = event.relative
+
+
 
