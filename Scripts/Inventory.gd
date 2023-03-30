b86ea7f4a818d308bd040027f4c4062729829de1
A-1 <a.arko06@gmail.com>
2023-03-30 16:53:20 +0300

    Inventory assets
    - Added inventory square 1
    - Added inventory square 2
    - Added inventory square 3

---
+++ Scripts/Inventory.gd
@@ -0,0 +1,21 @@
+extends Container
+
+var currentslot = 1
+
+func _ready():
+		$slot1.color = Color(0,0,0,0)
+		$slot2.color = Color(0,0,0,0)
+		$slot3.color = Color(0,0,0,0)
+func _physics_process(delta):
+	if Input.is_action_just_pressed("inv1"):
+		$slot1.color = Color(1,1,1,0.5)
+		$slot2.color = Color(0,0,0,0)
+		$slot3.color = Color(0,0,0,0)
+	if Input.is_action_just_pressed("inv2"):
+		$slot1.color = Color(0,0,0,0)
+		$slot2.color = Color(1,1,1,0.5)
+		$slot3.color = Color(0,0,0,0)
+	if Input.is_action_just_pressed("inv3"):
+		$slot1.color = Color(0,0,0,0)
+		$slot2.color = Color(0,0,0,0)
+		$slot3.color = Color(1,1,1,0.5)
