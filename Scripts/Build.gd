f20b5133f9819847731b33ac44886ce2232ce157
Marten Laane <marten.laane@hkhk.edu.ee>
2023-04-19 19:20:19 +0300

    Bugfix
    - Sang has now 2 dialouges
    - Delete cat after cutscene has ended
    - No more freecamsang bug
    - Compressed some if statements

--- Scripts/Build.gd
+++ Scripts/Build.gd
@@ -7,89 +7,43 @@ var time_in_seconds = 1
 var sang_cutscene = 0
 var cat_cutscene = 0
 
+var catcut2 = false
+var catcut3 = false
+
 var t = 0.0
 
-
 func _ready():
-	#eyes reapplied incase it doesnt
+	#eyes reapplied incase it doesnt DO IT ITSELF
 	$Character/KinematicBody/Head/Camera.current = true
 	
 func _physics_process(delta):
-#	print($kiisu.global_translation.y) #ignore, for kiisu pos debug if kiisu go retard
-#	print($kiisu.global_translation.x)
-	
 	#diagbox animatsioonid
 	if sang_cutscene == 0:
 		$Character/KinematicBody/InteractRay.set_physics_process(true)
 		if $Character/KinematicBody/InteractRay/Diabox.position.y < 1230:
 			$Character/KinematicBody/InteractRay/Diabox.position.y += 8
 			
-			
 	if sang_cutscene == 1:
 		$Character/KinematicBody/InteractRay.set_physics_process(false)
 		$Character/KinematicBody/InteractRay/InteractPrompt.text = " "
 		if $Character/KinematicBody/InteractRay/Diabox.position.y > 940:
 			$Character/KinematicBody/InteractRay/Diabox.position.y -= 8
-	
+		if Input.is_action_just_pressed("shank"):
+			$Character/KinematicBody/InteractRay/Diabox/RichTextLabel.text = "Kui sa tahad minu eest palvetada siis mine maja taha, annan sulle paar kommi kui lahed ka"
+			
+	#cat cutscene lerp for animation
 	if cat_cutscene == 1:
-#cut 1 ////////////////////////////////////////////////////////////////////////
-		$KiisuCutscene/Camera.current = true 
-		$Character/KinematicBody.global_translation = Vector3(1.8,0,0)
-		$Character/KinematicBody/InteractRay.hide()
-		$Character/KinematicBody.set_physics_process(false)
-		$Character/KinematicBody.set_process_input(false)
-		t = clamp(t, 0.0, 1.0)
-		
-		yield(get_tree().create_timer(time_in_seconds2), "timeout")
-#cut 2 ////////////////////////////////////////////////////////////////////////
-		$KiisuCutscene/Camera2.current = true 
-		t += 0.002
-		var munad = 1
-		
-		#ei tea kuidas lerp tootab niiet lets roll w it just works
-		# sexual offender check
-		#  kui offenses on olemas siis liiguta kassi
-		while munad == 1:
-			if get_node("kiisu") == null:
-				break
+		if catcut2 == true and is_instance_valid(get_node("/root/Spatial/kiisu")) == true:
+			t += 0.002
 			$kiisu.global_translation.y = lerp($kiisu.global_translation.y, 2, t)
-			yield(get_tree().create_timer(time_in_seconds), "timeout")
-			$kiisu.global_translation.y = 2 # it fixes pos
-			break
-		 # end check
-#cut 3 ////////////////////////////////////////////////////////////////////////
-		$KiisuCutscene/Camera3.current = true
-		t = 0
-		t += 0.1
-		
-		#be careful while loopidega, alati breaki need
-		#if not broken fps go kaboosh and/or game crash
-		while munad == 1:
-			if get_node("kiisu") == null:
-				break
+		if catcut3 == true and is_instance_valid(get_node("/root/Spatial/kiisu")) == true:
+			t = 0
+			t += 0.1
 			$kiisu.global_translation.x = lerp($kiisu.global_translation.x, 2, t)
-			yield(get_tree().create_timer(time_in_seconds), "timeout")
-			$kiisu.global_translation.x = 2
-			break
-		
-		#give legs and eyes back
-		$Character/KinematicBody/Head/Camera.current = true
-		$Character/KinematicBody/InteractRay.show()
-		$Character/KinematicBody.set_physics_process(true)
-		$Character/KinematicBody.set_process_input(true)
-		
-		#viska kiisu shadow realmi// todo: add show kiisu from cockposition of char that is invisible at first
-		$kiisu.hide()
-		$kiisu/kas/CollisionShape.set_deferred("disabled", true)
-		$kiisu/kas/Area/CollisionShape2.set_deferred("disabled", true)
-		cat_cutscene = 0
-		$Character/KinematicBody/dickcat.show()
-#done ////////////////////////////////////////////////////////////////////////
-		
+
 func _input(event):
 	if Input.is_action_just_pressed("secondary_interact"):
-		
-		#kui cutscene labi saab ss ta saab enda invaliidsusest yle
+		#kui cutscene katkestatakse saab ss ta saab enda invaliidsusest yle
 		sang_cutscene = 0
 		$Character.show()
 		$Character/KinematicBody.set_physics_process(true)
@@ -97,8 +51,8 @@ func _input(event):
 		$Character/KinematicBody/Head/Camera.current = true
 		
 func _on_InteractRay_SangCut():
-	
 	#char hakkab sangcuti parast invaliidiks ja kummituseks
+	$Character/KinematicBody/InteractRay/Diabox/RichTextLabel.text = "this is sample text you should ingnornetne this and listen to the teachings of our lord and savour sang sangman"
 	$Sang/SangArea/SangCam.current = true
 	$Character.hide()
 	$Character/KinematicBody.set_physics_process(false)
@@ -106,4 +60,41 @@ func _on_InteractRay_SangCut():
 	sang_cutscene = 1
 	
 func _on_InteractRay_CatScene():
-	 cat_cutscene = 1
+	cat_cutscene = 1
+	if cat_cutscene == 1:
+#cut 1 ////////////////////////////////////////////////////////////////////////
+		print("cut1")
+		$KiisuCutscene/Camera.current = true 									#cutscene cam 1
+		$Character/KinematicBody.global_translation = Vector3(1.8,0,0)			#character TP
+		$Character/KinematicBody/InteractRay.set_physics_process(false)			#interact prompt disabled
+		$Character/KinematicBody/InteractRay/InteractPrompt.text = " "
+		$Character/KinematicBody.set_physics_process(false)						#character movement disable
+		$Character/KinematicBody.set_process_input(false)
+		
+		t = clamp(t, 0.0, 1.0)													#lerp t value clamp
+		yield(get_tree().create_timer(time_in_seconds2), "timeout")				#sleep for 2 sec
+#cut 2 ////////////////////////////////////////////////////////////////////////
+		catcut2 = true															#enable lerp for cut 2
+		print("cut2")
+		$KiisuCutscene/Camera2.current = true 									#cut 2 camera
+		yield(get_tree().create_timer(time_in_seconds), "timeout")				#sleep for 1 sec
+		$kiisu.global_translation.y = 2 										#tp kiisu y axis to 2 for good measure
+		catcut2 = false															#disable lerp for cut 2
+#cut 3 ////////////////////////////////////////////////////////////////////////	
+		catcut3 = true															#enable lerp for cut 3
+		print("cut3")
+		$KiisuCutscene/Camera3.current = true									#cut 3 camera
+		yield(get_tree().create_timer(time_in_seconds2), "timeout")				#sleep for 2 sec
+		$kiisu.global_translation.x = 2											#tp kiisu x axis to 2 for good measure
+		catcut3 = false															#disable lerp for cut 3
+#finish ///////////////////////////////////////////////////////////////////////
+		$Character/KinematicBody/Head/Camera.current = true						#enable char cam
+		$Character/KinematicBody.set_physics_process(true)						#enable character movement
+		$Character/KinematicBody.set_process_input(true)
+		$Character/KinematicBody/dickcat.show()									#show cock
+		$kiisu.queue_free()														#delete kiisu node for memory
+		cat_cutscene = 0														#disable cat cutscene finally
+		$Character/KinematicBody/InteractRay/Achievemtn.text = "obTained C A T C O C K (patent pending)"
+		yield(get_tree().create_timer(5), "timeout")
+		$Character/KinematicBody/InteractRay/Achievemtn.text = ""
+		$Character/KinematicBody/InteractRay.set_physics_process(true)			#interact prompt enable
--- Scripts/InteractRay.gd
+++ Scripts/InteractRay.gd
@@ -22,9 +22,8 @@ func _physics_process(delta):
 		if  collider.is_in_group("NPC"):
 			$InteractPrompt.text = "Vajuta [E]"
 		
-		if is_instance_valid(get_node("/root/Spatial/kubik")) == true:
-			if collider.is_in_group("Kubik"):
-				$InteractPrompt.text = "Vajuta [E]"
+		if is_instance_valid(get_node("/root/Spatial/kubik")) == true and collider.is_in_group("Kubik"):
+			$InteractPrompt.text = "Vajuta [E]"
 			
 		if  collider.is_in_group("Kass"):
 			$InteractPrompt.text = "Vajuta [E]"
@@ -35,15 +34,14 @@ func _physics_process(delta):
 		if collider.is_in_group("Kass") and Input.is_action_just_pressed("interactt"):
 			emit_signal("CatScene")
 			
-		if is_instance_valid(get_node("/root/Spatial/kubik")) == true:	
-			if collider.is_in_group("Kubik") and Input.is_action_just_pressed("interactt"):
-				print("Kubik Pickup")
+		if is_instance_valid(get_node("/root/Spatial/kubik")) == true and collider.is_in_group("Kubik") and Input.is_action_just_pressed("interactt"):
+			print("Kubik Pickup")
 			#kubikinteract on queue_freemas said kubikut
-				emit_signal("KubikInteract")
+			emit_signal("KubikInteract")
 			
-				$Achievemtn.text = "Korjasid ules kubiku"
-				yield(get_tree().create_timer(time_in_seconds), "timeout")
-				$Achievemtn.text = " "
+			$Achievemtn.text = "Korjasid ules kubiku"
+			yield(get_tree().create_timer(time_in_seconds), "timeout")
+			$Achievemtn.text = " "
 			
 	else:
 		$InteractPrompt.text = "·"
