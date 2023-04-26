4cf6eedd8a4917286849670c865e34fe140df4a7
Marten Laane <marten.laane@hkhk.edu.ee>
2023-04-26 15:08:10 +0300

    knife bug fix
    - knifeiga saab interactida
    - lisatud paar tooltipi

--- Scripts/InteractRay.gd
+++ Scripts/InteractRay.gd
@@ -15,12 +15,22 @@ func _physics_process(delta):
 #	if Input.is_action_just_pressed("interact"):
 #		print("interacted")
 	var collider = get_collider()
-	$InteractPrompt.text = " "
+	
 	#NPC INTERACTION
 	
 	if collider != null:
 		
-		#Kui teed uue interactable objecti lisa siia if statementi, saveb ruumi. ~ A-1
+		#Kui teed uue interactable objecti lisa siia if statementi, saveb ruumi. ~ A-1		
+		#Iga kord kui uue inter_objecti lisad siis tee kindlaks, et selle 'Area' nodei collision layer on 4 peal //
+		#Kui kustutad objekti siis pane selle signal emit if statementi, et see teeks kindlaks, et node eksisteerib //
+		
+		#Layer annab Maskile infot ning vastupidi, aga layer ei saa layerile infot anda //
+		#aka kui object layer on 2 ja interactray mask on 2, siis sa saad interactida sellega //
+		#interactrayle saab ainult maski lisada, niiet jata see meelde //
+		
+		#rn Ground Mask = 1, kinematicbody layer = 1 //
+		#object layer = 4, interactray mask = 4 ~ p
+		
 		if  collider.is_in_group("NPC") || collider.is_in_group("Kubik") || collider.is_in_group("Knife") || collider.is_in_group("Kass"):
 			$InteractPrompt.text = "Vajuta [E]"
 
@@ -39,9 +49,12 @@ func _physics_process(delta):
 			yield(get_tree().create_timer(time_in_seconds), "timeout")
 			$Achievemtn.text = " "
 			
-		if collider.is_in_group("Knife") and Input.is_action_just_pressed("interact"):
+		if is_instance_valid(get_node("/root/Spatial/kubik")) == true and collider.is_in_group("Knife") and Input.is_action_just_pressed("interact"):
 			emit_signal("KnifeInteract")
-		if collider.is_in_group("Knife"):
-			print("KNIFE")
+			
+			$Achievemtn.text = "Korjasid ules noa"
+			yield(get_tree().create_timer(time_in_seconds), "timeout")
+			$Achievemtn.text = " "
+			
 	else:
 		$InteractPrompt.text = "·"
