a55b0d0c428c6f5aad6cdca8824ab38c79ed69fd
A-1 <a.arko06@gmail.com>
2023-05-04 20:21:03 +0300

    "better" main screen

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -2,9 +2,9 @@ extends Control
 
 func _ready():
 	pass
-
-func _on_Button_pressed():
+	
+func _on_Seikleme_pressed():
 	get_tree().change_scene("res://Scenes/Build.tscn")
 	
-func _on_Button2_pressed():
+func _on_Kohila_pressed():
 	get_tree().change_scene("res://Scenes/credits.tscn")
