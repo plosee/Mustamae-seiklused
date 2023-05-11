49773259f94de87028d18e04e0079c0c306b138a
A-1 <a.arko06@gmail.com>
2023-05-12 00:53:53 +0300

    new main menu
    who dis

---
+++ Scripts/newmain.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+
+func _on_start_pressed():
+	get_tree().change_scene("res://Scenes/Build.tscn")
+
+
+func _on_settings_pressed():
+	get_tree().change_scene("res://Scenes/credits.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_fullscreen_toggled(button_pressed):
+	OS.window_fullscreen = !OS.window_fullscreen
