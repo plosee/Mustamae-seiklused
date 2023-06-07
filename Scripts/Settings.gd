8f4cb552df6b9f3f0c76f3dbcb81b97c99ea9466
A-1 <a.arko06@gmail.com>
2023-06-07 19:39:47 +0300

    this shit is a mess
    basic stuff reimplemented

---
+++ Scripts/Settings.gd
@@ -0,0 +1,28 @@
+extends Control
+
+@onready var mastervol = $static/SettingsVbox/MasterSlider.value
+@onready var sfxvol = $static/SettingsVbox/SFXSlider.value
+@onready var musicvol = $static/SettingsVbox/MusicSlider.value
+var master_bus = AudioServer.get_bus_index("Master")
+var music_bus = AudioServer.get_bus_index("Music")
+var sfx_bus = AudioServer.get_bus_index("SFX")
+var fpscap = false
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+
+func _on_static_finished():
+	$static.play()
+
+
+
+func _on_master_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(master_bus, value)
+	if value == -30:
+		AudioServer.set_bus_mute(master_bus, true)
+	else:
+		AudioServer.set_bus_mute(master_bus,
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,11 +1,41 @@
 extends Control
 
+var versnr = RandomNumberGenerator.new()
+#IF YOU PUSH AN UPDATE TO LIVE CHANGE THIS TO TODAY'S DATE (DD.MM.YY) ALONG WITH update.txt
+var versdate = "31.05.23"
+var updateurl = "https://liivakast.hkhk.edu.ee/~mlaane/download/update.txt"
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	$Buttons/Update/UpdateBG.hide()
+
+func _process(delta):
+	pass
+	
+func _on_bg_finished():
+	$BG.play()
+	
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+
+func _on_settings_pressed():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_quit_pressed():
+	pass # Replace with function body.
+
+#update function currently broken
+func _on_update_pressed():
+	$Buttons/Update/UpdateRequest.request(updateurl)
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	var output = body.get_string_from_utf8()
+	if output == versdate:
+		$Buttons/Update/UpdateBG.show()
+	else:
+		$Buttons/Update.text = "Latest Version"
+func _on_yeah_pressed():
+	OS.shell_open("https://plose.itch.io/mustamagi")
+func _on_nah_pressed():
+	$Buttons/Update/UpdateBG.hide()
