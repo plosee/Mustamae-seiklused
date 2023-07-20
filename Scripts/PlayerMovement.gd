d4665ec75b69a6f545cf715ef1ef3f1d850ec36d
Marten Laane <113144588+plosee@users.noreply.github.com>
2023-07-20 14:04:13 +0300

    Movement reimplemented in godot 4.1
     - movement is now available with brand new features like headbobbing and nonbuggy camera shit
    - also a-1 vaata kuidas see model liigub kui mangus olla

---
+++ Scripts/PlayerMovement.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+const SPEED = 50
+const GRAV = 9.8
+
+
+# Called when the node eznters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
