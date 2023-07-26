836e94c9d3328c674830636038c7d8e2c31f1310
A-1 <a.arko06@gmail.com>
2023-07-26 12:45:11 +0300

    kimu algused

--- Scripts/inventory.gd
+++ Scripts/inventory.gd
@@ -9,18 +9,18 @@ func _ready():
 	$inv3.color = unselected
 
 func _unhandled_input(event):
-	if event.keycode==49:
-		$Global.currentslot = 1
-		$inv1.color = selected
-		$inv2.color = unselected
-		$inv3.color = unselected
-	elif event.keycode==50:
-		$Global.currentslot = 2
-		$inv2.color = selected
-		$inv1.color = unselected
-		$inv3.color = unselected
-	elif event.keycode==51:
-		$Global.currentslot = 3
-		$inv3.color = selected
-		$inv1.color = unselected
-		$inv2.color = unselected
+	if event is InputEventKey:
+		if event.keycode==49:
+			$inv1.color = selected
+			$inv2.color = unselected
+			$inv3.color = unselected
+		elif event.keycode==50:
+			$inv2.color = selected
+			$inv1.color = unselected
+			$inv3.color = unselected
+		elif event.keycode==51:
+			$inv3.color = selected
+			$inv1.color = unselected
+			$inv2.color = unselected
+	else:
+		pass
