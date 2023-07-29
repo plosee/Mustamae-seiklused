10fab097da7e23fd30b4d63f86a02e73f9ebc833
Marten Laane <113144588+plosee@users.noreply.github.com>
2023-07-29 20:45:23 +0300

    KUBIK ANIMATSIONS!!!!!
    - Added kubik animations! kimu enda 2500 puffi paari sekundiga ara!
    - issue: particled lopetavad tootamise kui alla vaadata natuke
    - fixed animation not being local to player scene

--- Scripts/Interacting.gd
+++ Scripts/Interacting.gd
@@ -17,13 +17,20 @@ func _process(delta):
 		# lopeta kimu particle
 		$CharacterBody3D/Head/GPUParticles3D.emitting = false
 		
+	if Global.currentslot == 1 && Global.KubikPickup == true && Input.is_action_just_pressed('interactM1'):
+		# mangi puff animatsiooni kui m1 vajutatakse
+		$CharacterBody3D/Head/arms/RootNode/vasak/AnimationPlayer.play("vasak-Puff")
+		
 	if Global.currentslot == 1 && Global.KubikPickup == true && Input.is_action_pressed('interactM1'):
+		# lopeta puffimine kui samal ajal kimub
+		# kimu signal addib kui kaua puffida saab // hard limit
 		kimu.emit()
 		Global.KimuSmoke = false
-		# lopeta particled kui samal ajal kimud
+
 	if Global.KimuPuffs > 1 && Global.KubikPickup == true && Input.is_action_just_released("interactM1"):
 		Global.KimuSmoke = true
 		$CharacterBody3D/Head/GPUParticles3D.emitting = true
+		$CharacterBody3D/Head/arms/RootNode/vasak/AnimationPlayer.play("vasak-PuffStop")
 		# naita particle kuna hingab valja
 		
 	# Syringe kood
@@ -36,3 +43,9 @@ func _process(delta):
 	# Nuga kood
 	if Global.currentslot == 2 && Global.KnifePickup == true && Input.is_action_just_pressed('interactM1'):
 		stab.emit()
+
+
+func _on_animation_player_animation_finished(anim_name):
+	# puffidle animatsioon kui esimene anim saab labi
+	if anim_name == "vasak-Puff":
+		$CharacterBody3D/Head/arms/RootNode/vasak/AnimationPlayer.play("vasak-PuffIdle")
--- Scripts/arms.gd
+++ Scripts/arms.gd
@@ -1,10 +1,11 @@
 extends Node3D
 
-
+# kui midagi vajutatakse
 func _unhandled_input(event):
 	if event is InputEventKey:
-		
 		if event.keycode==49 || event.keycode==50 || event.keycode==51:
-			$RootNode/vasak/AnimationPlayer.play("parem|HandEquip")
-		elif event.keycode==49 && Global.KubikPickup == true:
-			$RootNode/vasak/kubik.visible = true
+			$RootNode/vasak/AnimationPlayer.play("vasak-HandEquip")
+			# equip animatsioon kui inv slotte vajutatakse
+		if Global.currentslot == 1 && Global.KubikPickup == true:
+			# kubik visible kui kubik inventorys
+			$RootNode/vasak/kubikhand.visible = true
